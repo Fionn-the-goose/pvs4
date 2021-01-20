eddf4538075e4d82e8bd7f1a565b78b676750174
PeteTheN00b <ropeter63@googlemail.com>
2021-01-20 21:57:51 +0100

    JOJO
    dududududududu
    GOLDEN WIND

--- PvSProject/PvSProject/helloWorld.cpp
+++ PvSProject/PvSProject/helloWorld.cpp
@@ -6,54 +6,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DATA_SIZE   10                          //prepare matrix size
+#include <chrono> //using this for sequential version speed test
+
+#define DATA_SIZE   1000                         //prepare matrix size
 #define MEM_SIZE    DATA_SIZE * DATA_SIZE * sizeof(float)   //prepares memory needed to store a matrix of given size containing float values
 
 /** kernel's source text as string **/
 const char* KernelSource =
-"#define DIM 10 // Groesse der Matrix \n"
-"__kernel void matmult(__global float *A, __global float *B, __global float *C) { \n"
-" int i, j, k; \n"
-" float sum; // Private Variable für Zwischenergebnisse \n"
-" i = get_global_id(0); \n"
-" for (j = 0; j < DIM; j++) { \n"
-" sum = 0.0; \n"
-" for (k = 0; k < DIM; k++) \n"
-" sum += A[i*DIM+k] * B[k*DIM+j]; \n"
-" C[i*DIM+j] = sum; \n"
-" } \n"
-"} \n"
-"\n";
-//"#define DIM 10 // Groesse der Matrix \n"
-//"__kernel void matmult(__global float *A, __global float *B, __global float *C) { \n"
-//" int k, j, i = get_global_id(0); \n"
-//" float Al[DIM], Bl[DIM], sum; \n"
-//" for (k = 0; k < DIM; k++) \n"
-//" Al[k] = A[i*DIM+k]; \n"
-//" for (j = 0; j < DIM; j++) { \n"
-//" for (k = 0; k < DIM; k++) \n"
-//" Bl[k] = B[k*DIM+j]; \n"
-//" sum = 0.0; \n"
-//" for (k = 0; k < DIM; k++) \n"
-//" sum += Al[k] * Bl[k]; \n"
-//" C[i*DIM+j] = sum; \n"
-//" } \n"
-//"} \n"
-//"\n";
 
-//"#define DATA_SIZE 10\n"
-//"__kernel void matmult(__global float *Ap, __global float *Bp, __global float *Cp)\n"
-//"{\n"
-//"int i, j, k;\n"
-//"i = get_global_id(1);\n"
-//"j = get_global_id(0);\n"
-//"for(k = 0; k < DATA_SIZE; ++k)\n"
-//"	sum += A[i*DIM + k] * B[k*DIM + j];\n"
-//"C[i*DIM+j] = sum;\n"
-//"}\n"
-//"\n";
+"#define DATA_SIZE 1000																	\n"
+"__kernel void matmult(__global float* Ap, __global float* Bp, __global float* Cp)		\n"
+"{																						\n"
+"	int i, j, k;																		\n"
+"	float sum = 0.f;																	\n"
+"	i = get_global_id(0);																\n" //past a certain threshhold of matrix size, doubling the matrix size causes an eightfold increase in compile time, suggesting that we eventually reach the maximum possible parallelization and return to a structure equivalent to 3 nested loops, up until that point it is significantly faster though (for us this happened when going from size 1000 to 2000)
+"	j = get_global_id(1);																\n"
+"	for (k = 0; k < DATA_SIZE; ++k)														\n"
+"	{																					\n"
+"		sum += Ap[i * DATA_SIZE + k] * Bp[k * DATA_SIZE + j];							\n"
+"	}																					\n"
+"	Cp[i * DATA_SIZE + j] = sum;														\n"
+"}																						\n"
+"																						\n";
 
-//"#define DATA_SIZE 10												\n"
+//"#define DATA_SIZE 3												\n"
 //"__kernel void test(__global float *input, __global float *input2, __global float *output)  \n"
 //"{																	\n"
 //"	size_t i = get_global_id(0);									\n"
@@ -79,6 +55,12 @@ void init_mat(float** A, int row, int col)
 		A[0][i] = (float)(rand() % 10);
 }
 
+void init_zero(float** A, int row, int col)
+{
+	for (int i = 0; i < row * col; i++)
+		A[0][i] = 0;
+}
+
 void print_mat(float** A, int row, int col, char const* tag)
 {
 	int i, j;
@@ -92,10 +74,49 @@ void print_mat(float** A, int row, int col, char const* tag)
 	}
 }
 
+void free_mat(float** A, int num_rows) {
+	free(A[0]);
+	free(A);
+}
+
+bool compare_mat(float** A, float** B, int row, int col) {
+	for(int i = 0; i < row; ++i)
+		for (int j = 0; j < col; ++j)
+		{
+			if (A[i][j] != B[i][j]) return false; //iterate through all elements, if any are different return that they are not equal
+		}
+
+	return true; //if we reached this point we haven't found any differences as we would have otherwise returned false already, so we can return true
+}
+
 
 /** Body of the main code **/
 int main(void)
 {
+	//prepare matrices
+	float** A = alloc_mat(DATA_SIZE, DATA_SIZE); init_mat(A, DATA_SIZE, DATA_SIZE);
+	float** B = alloc_mat(DATA_SIZE, DATA_SIZE); init_mat(B, DATA_SIZE, DATA_SIZE);
+	float** serialC = alloc_mat(DATA_SIZE, DATA_SIZE);
+	//Serial variant in here
+	{
+		std::chrono::milliseconds start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+		int i, j, k;
+		for (i = 0; i < DATA_SIZE; i++)
+			for (j = 0; j < DATA_SIZE; j++)
+				for (k = 0; k < DATA_SIZE; k++)
+					serialC[i][j] += A[i][k] * B[k][j];
+
+		std::chrono::milliseconds end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+		printf("\nSerial Time Taken in Milliseconds: %lld\n\n\n", end.count() - start.count());
+	}
+	//Everything past here is for the open cl version
+
+
+
+
+
 	cl_int				err;                      //stores information about success or failure of commands
 	cl_platform_id* platforms = NULL;         //id of the platform
 	char			    platform_name[1024];      //name of the platform
@@ -107,10 +128,13 @@ int main(void)
 	cl_command_queue	command_queue;            //queue storing commands
 	cl_program 			program;                  //stores generated program
 	//cl_mem				input, input2, output;            //stores necessary available memory
-	float				data[DATA_SIZE] =         //prepare storage for data
-	{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	size_t				global[1] = { DATA_SIZE };  //global storage for data size
+	//float				data[DATA_SIZE] =         //prepare storage for data
+	//{ 1, 2, 3 , 4, 5, 6, 7, 8, 9, 10};
+	size_t				global[2] = { DATA_SIZE, DATA_SIZE };  //global storage for data size
 	float				results[DATA_SIZE] = { 0 }; //empty storage with enough space for future matrix data
+
+	cl_event event;
+	cl_ulong start, end;
 
 	/* 1) */
 
@@ -171,7 +195,7 @@ int main(void)
 	}
 
 	// Creates a queue (FIFO)
-	command_queue = clCreateCommandQueue(context, device_id, 0, &err);
+	command_queue = clCreateCommandQueue(context, device_id, CL_QUEUE_PROFILING_ENABLE, &err);
 	if (err != CL_SUCCESS)
 	{
 		printf("Unable to create command queue. Error: %d\n", err);
@@ -188,6 +212,20 @@ int main(void)
 
 	// Compile and link the kernel source text
 	err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (err == CL_BUILD_PROGRAM_FAILURE) { //copied from https://stackoverflow.com/questions/9464190/error-code-11-what-are-all-possible-reasons-of-getting-error-cl-build-prog for debugging purposes, no effect on the functional code
+		// Determine the size of the log
+		size_t log_size;
+		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+
+		// Allocate memory for the log
+		char* log = (char*)malloc(log_size);
+
+		// Get the log
+		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+
+		// Print the log
+		printf("%s\n", log);
+	}
 	if (err != CL_SUCCESS)
 	{
 		printf("Error building program. Error: %d\n", err);
@@ -195,7 +233,7 @@ int main(void)
 	}
 
 	// Specify kernel
-	kernel = clCreateKernel(program, "test", &err);
+	kernel = clCreateKernel(program, "matmult", &err);
 	if (err != CL_SUCCESS)
 	{
 		printf("Error setting kernel. Error: %d\n", err);
@@ -205,19 +243,20 @@ int main(void)
 
 	/* 2) */
 
-	float** A, ** B, ** C;
+	//float** A, ** B;
+	float** C;
 	cl_mem Ap, Bp, Cp;
 
-	A = alloc_mat(DATA_SIZE, DATA_SIZE); init_mat(A, DATA_SIZE, DATA_SIZE);
-	B = alloc_mat(DATA_SIZE, DATA_SIZE); init_mat(B, DATA_SIZE, DATA_SIZE);
+	//A = alloc_mat(DATA_SIZE, DATA_SIZE); init_mat(A, DATA_SIZE, DATA_SIZE);
+	//B = alloc_mat(DATA_SIZE, DATA_SIZE); init_mat(B, DATA_SIZE, DATA_SIZE);
 	C = alloc_mat(DATA_SIZE, DATA_SIZE);
 
 	Ap = clCreateBuffer(context, CL_MEM_READ_ONLY, MEM_SIZE, NULL, &err);
 	Bp = clCreateBuffer(context, CL_MEM_READ_ONLY, MEM_SIZE, NULL, &err);
 	Cp = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE, NULL, &err);
 
-	clEnqueueWriteBuffer(command_queue, Ap, CL_TRUE, 0, DATA_SIZE, A[0], 0, NULL, NULL);
-	clEnqueueWriteBuffer(command_queue, Bp, CL_TRUE, 0, DATA_SIZE, B[0], 0, NULL, NULL);
+	clEnqueueWriteBuffer(command_queue, Ap, CL_TRUE, 0, MEM_SIZE, A[0], 0, NULL, NULL);
+	clEnqueueWriteBuffer(command_queue, Bp, CL_TRUE, 0, MEM_SIZE, B[0], 0, NULL, NULL);
 
 	clSetKernelArg(kernel, 0, sizeof(cl_mem), &Ap);
 	clSetKernelArg(kernel, 1, sizeof(cl_mem), &Bp);
@@ -241,7 +280,7 @@ int main(void)
 	/* 3)  */
 
 	// Puts kernel into command queue and splits up instructions
-	clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, global, NULL, 0, NULL, NULL);
+	clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global, NULL, 0, NULL, &event);
 
 	// Wait for queue to complete
 	clFinish(command_queue);
@@ -249,14 +288,20 @@ int main(void)
 	// Read and store results of output buffer into results
 	//clEnqueueReadBuffer(command_queue, output, CL_TRUE, 0, MEM_SIZE, results, 0, NULL, NULL);
 	clEnqueueReadBuffer(command_queue, Cp, CL_TRUE, 0, MEM_SIZE, C[0], 0, NULL, NULL);
-
-
 	// Display results
 	//for (unsigned int i = 0; i < DATA_SIZE; i++)
 	//	printf("%f\n", results[i]);
-	print_mat(C, DATA_SIZE, DATA_SIZE, "C");
 
+	//print_mat(A, DATA_SIZE, DATA_SIZE, "A");
+	//print_mat(B, DATA_SIZE, DATA_SIZE, "B");
+	//print_mat(C, DATA_SIZE, DATA_SIZE, "C");
+	//print_mat(serialC, DATA_SIZE, DATA_SIZE, "sC");
 
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(start), &start, NULL);
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(end), &end, NULL);
+	printf("OpenCL time = %.1f ms\n", ((end - start) / 1000000.0));
+
+	printf("Matrices are %s", compare_mat(C, serialC, DATA_SIZE, DATA_SIZE) ? "equal" : "not equal");
 
 	/* 4) */
 	clReleaseMemObject(Ap);
@@ -268,6 +313,11 @@ int main(void)
 	clReleaseKernel(kernel);
 	clReleaseCommandQueue(command_queue);
 	clReleaseContext(context);
+
+	free_mat(A, DATA_SIZE);
+	free_mat(B, DATA_SIZE);
+	free_mat(C, DATA_SIZE);
+	free_mat(serialC, DATA_SIZE);
 
 	return 0;
 }
